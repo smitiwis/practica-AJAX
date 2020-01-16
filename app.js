4bb003bd70b6159cd3ff39820c8e6111f57d5200
smitiwis <sistemas.luismi@gmail.com>
2020-01-16 18:23:34 -0500

    terminado

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 var etiquetaDiv = document.getElementById("info");
 
 var ajax = new XMLHttpRequest();
-console.log(ajax)
+
+function mostrarSugerencia(txtValor) {
+    if (txtValor.length === 0) {
+        document.getElementById("info").innerHTML = "";
+    } else {
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState === 4 && ajax.status === 200) {
+                etiquetaDiv.innerHTML = ajax.responseText;
+            }
+        };
+        ajax.open("GET", "servidor.php?nombre=" + txtValor, true);
+        ajax.send();
+    }
+}
--- servidor.php
+++ servidor.php
@@ -3,29 +3,54 @@ $mysqli = new mysqli("localhost", "root", "", "colegio");
 if ($mysqli->connect_errno) {
     echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }else{
-    echo "Conexion exitosa <br>";
     $resultado = $mysqli->query("SELECT * FROM alumno");
     $arrayNombres = [];
     for ($num_fila = $resultado->num_rows - 1 ; $num_fila >= 0 ; $num_fila--) { 
         $resultado->data_seek($num_fila);
         $fila = $resultado->fetch_assoc();
-        $listaNombres = $fila['nombre'];
-        array_push($arrayNombres, $listaNombres);
-    } 
-}
-$nombre = $_POST['nombre'];
-$sugerencia = "";
-if($nombre !== ""){
-    $nombre = strtolower($nombre);
-    foreach($arrayNombres as $personas){
-        $nombreEnServidor = substr($personas, 0, $cantidadCaracteres);
-        if (stristr($nombre, $nombreEnServidor) !== false) {
-            if ($sugerencia === "") {
-                $sugerencia = $personas;
-            }else{
-                $sugerencia .= ", $personas";
+        $Nombres = $fila['nombre'];
+        array_push($arrayNombres, $Nombres);
+    }
+
+    $nombre = $_GET['nombre'];
+    $sugerencia = "";
+    if ($nombre !== "") {
+        //convertir a minusculas
+        $nombre= strtolower($nombre);
+        $cantidadDecaracteres = strlen($nombre);
+        foreach($arrayNombres as $persona){
+            $nombreEnServidor = substr($persona, 0, $cantidadDecaracteres);
+            if (stristr($nombre, $nombreEnServidor) !== false) {
+                if ($sugerencia === "") {
+                    $sugerencia = "<li> $persona</li>";
+                }else{
+                    $sugerencia .= "<li> $persona</li>";
+                }
             }
         }
     }
+    echo $sugerencia === "" ? "<li>No fue encontrado</li>" : $sugerencia; 
+    
 }
+
+/* REPASANDO CON FUNCIONES PARA CADESNAS
+-----> $cadena = "Los colores primarios"; <------
+substr($cadena, 5);
+    como resutado obtenemos  =>  olores primarios
+_______________________________________________________
+substr($cadena, 5, 6);
+    Como resultado obtenemos => olores
+_______________________________________________________
+stristr($cadena, "res");
+ahora ver si una cadena está dentro de otra cadena o no
+    como resultado obtenemos => res primarios
+    AHORA APLICAMOS EN UNA IF:
+    if(stristr($cadena, "res")){
+        echo "res  ha sido encontrado en la cadena";
+    }else{
+        echo "res no fue encontrado en la cadena";
+    }
+_______________________________________________________
+ */
+
 ?>
